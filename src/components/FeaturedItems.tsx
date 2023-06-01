b2b1db69dfbb8ea6d75ba41ebe1cde190021eb0f
WilliamTrodd <william.trodd@gmail.com>
2023-06-01 10:48:22 +0300

    tweaked View All button

--- src/components/FeaturedItems.tsx
+++ src/components/FeaturedItems.tsx
@@ -11,15 +11,15 @@ export const FeaturedItems: Component<FeaturedItemsProps> = (props) => {
 
   return (
 
-    <div class='flex flex-col items-center'>
+    <div class='flex flex-col'>
     <h2 class='font-serif text-md font-semibold my-3 pl-3 tracking-wider'>Featured Items</h2>
     <div class='grid grid-cols-2 md:grid-cols-4 md:w-4/5 md:mx-auto gap-1 px-3'>
     <For each={state.products} fallback={<div>Loading...</div>}>
       {item => <FeaturedProductCard product={item} />}
     </For>
     </div>
-    <A href='/shop' class='px-4 py-2 mt-2 hover:scale-105 font-serif bg-accent-green'>
-      View All
+    <A href='/shop' >
+      <div class='w-fit mx-auto px-4 py-2 mt-2 hover:scale-105 font-serif bg-accent-green'>View All</div>
     </A>
     </div>
   )
