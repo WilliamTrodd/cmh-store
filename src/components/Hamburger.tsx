24f43f390a68f63b1e2614c57e87888fd5e22bdf
WilliamTrodd <william.trodd@gmail.com>
2023-06-01 08:47:04 +0300

    cleaned up hamburger implementation

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -1,17 +1,13 @@
-import { Component, createSignal } from 'solid-js'
-import {state, setState} from '../store'
+import { Component } from 'solid-js'
+import {state} from '../store'
 
 export const Hamburger: Component = () => {
 
-  const handleClick = () => {
-    setState('hbOpen', !state.hbOpen)
-  }
-
   return (
-    <div id='hb' class='mx-4 w-12 cursor-pointer' onClick={handleClick}>
-      <span id='hb-top' class='block w-6 h-1 bg-accent-green mb-1 transition-all duration-200 ease-in' classList={{['rotate-45 translate-y-2']: state.hbOpen}}></span>
-      <span id='hb-mid' class='block w-6 h-1 bg-accent-green mb-1 transition-all duration-200 ease-in' classList={{['opacity-0']: state.hbOpen}}></span>
-      <span id='hb-bot' class='block w-6 h-1 bg-accent-green transition-all duration-200 ease-in' classList={{['-rotate-45 -translate-y-2']: state.hbOpen}}></span>
+    <div id='hb' class='mx-4 w-12 cursor-pointer'>
+      <span id='hb-top' class='block w-6 h-1 bg-accent-green mb-1 transition-all duration-200 ease-in' classList={{['rotate-45 translate-y-2']: state.menuOpen}}></span>
+      <span id='hb-mid' class='block w-6 h-1 bg-accent-green mb-1 transition-all duration-200 ease-in' classList={{['opacity-0']: state.menuOpen}}></span>
+      <span id='hb-bot' class='block w-6 h-1 bg-accent-green transition-all duration-200 ease-in' classList={{['-rotate-45 -translate-y-2']: state.menuOpen}}></span>
     </div>
   )
 }
