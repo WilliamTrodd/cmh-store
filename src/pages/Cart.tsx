e3eb48ee86d15fd6a9ed122f41dbbb895a8a3f4f
WilliamTrodd <william.trodd@gmail.com>
2023-06-01 10:48:02 +0300

    Set up empty cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,12 +1,17 @@
 import { Component } from "solid-js"
+import { FeaturedItems } from "../components/FeaturedItems"
 
 const Cart: Component = () => {
 
 
   return (
-    <div>
-      
+    <>
+    <div class='flex flex-col items-center mt-3 mb-5'>
+      <h2 class='text-2xl font-serif mb-6'>Your cart is empty</h2>
+      <div class='bg-accent-green text-sm font-thin font-serif px-5 py-2'>Continue shopping</div>
     </div>
+    <FeaturedItems productIds={[]}/>
+    </>
   )
 }
 
